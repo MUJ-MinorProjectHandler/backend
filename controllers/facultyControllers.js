4e6bd70128360906941e2897c9491acd0041b30f
devagra28 <114013874+devagra28@users.noreply.github.com>
2023-12-02 00:01:28 +0530

    Update facultyControllers.js

--- controllers/facultyControllers.js
+++ controllers/facultyControllers.js
@@ -370,7 +370,7 @@ exports.studentAccept = async (req, res) => {
         text: `${studentinfo.name},
           Your application to work under ${facultyinfo.name} has been accepted. The Faculty will contact you for further details
           
-          Category:${category}
+          Category: ${category}
 
           
           
