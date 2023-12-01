4e9b80e7fe9b7a4f984721b1261bce1b8a01d1cc
devagra28 <114013874+devagra28@users.noreply.github.com>
2023-12-02 00:23:18 +0530

    Update adminControllers.js

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -31,7 +31,7 @@ exports.rejectStudent = async(req, res) =>{
         {
           $set: {
             status: "pending",
-            category: "-"
+            category: "-",
             faculty_mail: "_mail_",
             faculty_name: "_name_",
           },
